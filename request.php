977f6b4a7233bad0352206c6d8b32a5561fe26c5
Sabbir <s.sabbirhossain18@gmail.com>
2023-11-26 22:09:41 +0600

    request php fronted bug fixed

--- request.php
+++ request.php
@@ -2,10 +2,7 @@
 session_start(); 
 include('db.php');
 
-
 ?>
-
-
 
 
 <!DOCTYPE html>
@@ -13,61 +10,83 @@ include('db.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RequestPage</title>
+    <title>UIU Fydp Horizon</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="request.css">
+
+    <style>
+        <style>
+
+        .table th, .table td {
+            text-align: center;
+        }
+        .navbar-custom {
+            background-color: #42b1fa;
+        }
+        .navbar-brand {
+            text-align: center;
+            width: 100%;
+            color: #ffffff; 
+        }
+        body {
+            background-color: #f8f9fa; 
+        }
+        .container {
+            background-color: #ffffff; 
+            border-radius: 8px;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .d-flex{
+            margin-left: 850px;
+        }
+
+        
+        input#searchInput {
+        height: 40px;
+        margin-top: -48px;
+        margin-left: 250px;
+        }
+
+    </style>
 </head>
 
 <body>
+   
+<nav class="navbar navbar-dark navbar-custom fixed-top">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#" style=" background-color: transparent;"><h3>UIU Fydp Horizon</h3></a>
 
-    <div id="main_div">
+        <!-- Updated search form -->
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="searchInput">
+        </form>
 
-        <div class="header">
-            <a href="https://www.uiu.ac.bd/" class="logo"><img width="65px" src="uiu_logo.png"></a>
-            <a href="#fydpHorizon" class="name">UIU FYDP Horizon</a>
-    
-            <div class="header-right">
-              <a class="active" href="#home"><img width="30px" src="request.png"> Requests</a>
-              <a href="#home"><img width="30px" src="mess.webp"> Messages</a>
-              <a href="#home"><img width="30px" src="images.png"> Profile</a>
+        <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav flex-grow-1 ps-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="homepage.php">Home</a>
+                        <a class="nav-link active" aria-current="page" href="#">Publish Paper</a>
+                        <a class="nav-link active" aria-current="page" href="Facultylist.php">Mentors</a>
+                        <a class="nav-link active" aria-current="page" href="Note.php">Note</a>
+                        <a class="nav-link active" aria-current="page" href="#">Notifications</a>
+                        <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
         </div>
+    </div>
+</nav>
+    <br> <br><br><br><br>
 
-
-        <div class="left_div">
-            <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menu</span>
-
-            <div id="mySidenav" class="sidenav">
-               <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                <br><br><br><br><br><br>
-                <a href="homepage.php">Home</a>
-                <a href="apply.php">Application</a>
-                <a href="#">Published Papers</a>
-                <a href="Facultylist.php">Faculties</a>
-                <a href="request.php">Requests</a>
-                <a href="#">Notifications</a>
-                <a href="group.php">Create Group</a>
-                <br><br><br><br><br><br><br><br>
-                <a href="#">Settings</a>
-            </div>
-
-            <script>
-                function openNav() {
-                  document.getElementById("mySidenav").style.width = "250px";
-                  document.getElementById("mainDiv").style.marginLeft = "250px";
-                  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-                }
-                
-                function closeNav() {
-                  document.getElementById("mySidenav").style.width = "0";
-                  document.getElementById("mainDiv").style.marginLeft= "0";
-                  document.body.style.backgroundColor = "white";
-                }
-            </script>
-        </div>
-
-
-
-        <div class="form_div">
             <div class="square">
                 
                 <table class="table">
@@ -153,12 +172,33 @@ include('db.php');
             </div>
         </div>
 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            // Get the table and search input element
+            const facultyTable = document.getElementById('facultyTable');
+            const searchInput = document.getElementById('searchInput');
 
-        
+            // Add event listener for the search input
+            searchInput.addEventListener('input', function () {
+                const searchText = searchInput.value.toLowerCase();
 
+                // Loop through each row in the table body
+                const rows = facultyTable.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+                for (let i = 0; i < rows.length; i++) {
+                    const row = rows[i];
+                    const facultyName = row.getElementsByTagName('td')[0].innerText.toLowerCase();
+                    const facultyField = row.getElementsByTagName('td')[1].innerText.toLowerCase();
 
-
-    </div>
-    
+                    // Show or hide the row based on the search text in name or field
+                    if (facultyName.includes(searchText) || facultyField.includes(searchText)) {
+                        row.style.display = '';
+                    } else {
+                        row.style.display = 'none';
+                    }
+                }
+            });
+        });
+    </script>
 </body>
 </html>
