dac0008a577edbe5d1b3ccf694a3af77ece1a30b
Sabbir <s.sabbirhossain18@gmail.com>
2023-11-30 18:57:21 +0600

    Teacher Group Page Created

---
+++ teacherGroup.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UIU Fydp Horizon</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="teacherGroup.css">
+    
+</head>
+<body>
+    
+   <div>
+    <?php include('navbar.php'); ?>
+   </div>
+    <br> <br><br><br><br>
+
+        <div class="center_div1">
+    
+             <div class="squareS">
+               <div class="upperdiv">
+               <div class="progress" style="width: 70%;float: left;">
+            <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+            
+         </div>
+         <div class="button-container"  style="width: 27%;">
+            <button class="button button1" style="background-color: rgb(47, 189, 47);" >Agree</button>
+            <button class="button button1" style="background-color: rgb(29, 29, 231);">Disagree</button>
+         </div>
+               </div>
+         <!-- Progress Bar -->
+       
+         <!-- Agree and Disagree Buttons on the same line -->
+         
+         <!-- Readonly Textbox with border and shadow -->
+         <div class="middlediv" style="height: 200px;  border: 1px solid #dddddd80;
+         box-shadow: 3px 3px 18px rgba(0, 0, 0, 0.3); ">
+         
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna nec elit aliquam venenatis.</p>
+      
+         </div>
+         
+         <!-- Scrollable Text Input Box and Send Button -->
+         <div class="lowerdiv">
+         <div style="width: 85%;float: left;">
+            <textarea class="scrollable-textbox" style="height: 150px;" placeholder="Type your message..."></textarea>
+            
+         </div>
+         <div style="width: 5%;float: left;">
+            <button class="button button2" style="background-color:blue ">Send</button>
+         </div>
+         
+      </div>
+           
+        </div>
+    </div>
+
+
+</body>
+
+</html>
